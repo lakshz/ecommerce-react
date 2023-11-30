b62d4092e1d00976080b1a3106e7decdaf5a6ef4
Lakshya Satpal <lakshyasatpal372@gmail.com>
2023-11-30 09:43:40 +0530

    added portal on modal component

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,17 +1,19 @@
 import React from "react";
 import "./Modal.css";
+import { createPortal } from "react-dom";
 
 const Modal = ({ children, show, onClose }) => {
   if (!show) {
     return null;
   }
 
-  return (
+  return createPortal(
     <div className="backdrop" onClick={onClose}>
       <div className="modal" onClick={(e) => e.stopPropagation()}>
         {children}
       </div>
-    </div>
+    </div>,
+    document.body
   );
 };
 
